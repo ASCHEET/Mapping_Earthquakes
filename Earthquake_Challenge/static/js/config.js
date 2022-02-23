ba78f84a010ce69b1dd02a5501d94302c594a010
ASCHEET <88494881+ASCHEET@users.noreply.github.com>
2022-02-23 16:00:35 -0600

    config key

--- Earthquake_Challenge/static/js/config.js
+++ Earthquake_Challenge/static/js/config.js
@@ -1,6 +1,4 @@
 // API key
 
-const k1 = "pk.eyJ1IjoiYXNjaGVldCIsImEiO";
-const k2 = "iJja3lreWs2cWgyM3lhMm9y";
-const k3 = "c213NmJ3dzNhIn0.DDFxo2evGs7V4t5Qnc0pXA";
+const k1 = "pk.eyJ1IjoiYXNjaGVldCIsImEiOiJja3lreWs2cWgyM3lhMm9yc213NmJ3dzNhIn0.DDFxo2evGs7V4t5Qnc0pXA";
 
