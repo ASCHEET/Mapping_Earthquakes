c061f6d4ce97a551ca965695b61b21c85d7bc228
ASCHEET <88494881+ASCHEET@users.noreply.github.com>
2022-02-23 15:41:06 -0600

    config

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -5,21 +5,21 @@ console.log("working");
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken: API_KEY
+	accessToken: k1+k2+k3
 });
 
 // Create the second tile layer that will be the background of the map
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken: API_KEY
+	accessToken: k1+k2+k3
 });
 
 // Create the third tile layer that will be the background of the map
 let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken: API_KEY
+	accessToken: k1+k2+k3
 });
 
 // Create the map object with center, zoom level and default layer.
--- Earthquake_Challenge/static/js/config.js
+++ Earthquake_Challenge/static/js/config.js
@@ -1,2 +1,6 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiYXNjaGVldCIsImEiOiJja3lreWs2cWgyM3lhMm9yc213NmJ3dzNhIn0.DDFxo2evGs7V4t5Qnc0pXA";
+
+const k1 = "pk.eyJ1IjoiYXNjaGVldCIsImEiO";
+const k2 = "iJja3lreWs2cWgyM3lhMm9y";
+const k3 = "c213NmJ3dzNhIn0.DDFxo2evGs7V4t5Qnc0pXA";
+
