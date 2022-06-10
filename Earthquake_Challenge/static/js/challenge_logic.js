28cf88a7351ac79aa8417687858860376afcd94e
ASCHEET <88494881+ASCHEET@users.noreply.github.com>
2022-06-10 14:00:32 -0500

    Update challenge_logic.js

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -74,21 +74,21 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 8) {
-      return "#ea2c2c";
+      return "#ff4000";
     }
-    if (magnitude > 6.5) {
-      return "#ea822c";
+    if (magnitude > 6) {
+      return "#ff8000";
     }
     if (magnitude > 4) {
-      return "#ee9c00";
+      return "#ffbf00";
+    }
+    if (magnitude > 3) {
+      return "#a6ff00";
     }
     if (magnitude > 2) {
-      return "#eecc00";
+      return "#d9ff00";
     }
-    if (magnitude > 1) {
-      return "#d4ee00";
-    }
-    return "#98ee00";
+    return "#fff700";
   }
 
   // This function determines the radius of the earthquake marker based on its magnitude.
@@ -112,7 +112,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
      //  after the marker has been created and styled.
      onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Date/Time: " + feature.properties.time);
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Date/Time: " + timeConverter(feature.properties.time));
     }
   }).addTo(allEarthquakes);
 
@@ -131,12 +131,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
     return time;
   }
-  console.log(timeConverter(1652385567000));
+  console.log(timeConverter(1654541251000));
 
 
   // 3. Retrieve the major earthquake GeoJSON data > 4.5 mag for the week
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
-
+// d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // 4. Use the same style as the earthquake data
   function styleInfo(feature) {
     return {
@@ -152,14 +152,23 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake
   function getColor(magnitude) {
-    if (magnitude > 5) {
-      return "#922a31";
+    if (magnitude > 8) {
+      return "#ff4000";
     }
-    if (magnitude > 4) {
-      return "#ffa500";
+    if (magnitude > 6) {
+      return "#ff8000";
     }
     if (magnitude < 4) {
-      return "#ffff00"
+      return "#ffbf00"
+    }
+    if (magnitude < 3) {
+      return "#a6ff00"
+    }
+    if (magnitude < 2) {
+      return "#d9ff00"
+    }
+    if (magnitude < 1) {
+      return "#fff700"
     }
   }
   
@@ -184,9 +193,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
 
     // circleMarker popups
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Date/Time: " + feature.properties.time);
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Date/Time: " + timeConverter(feature.properties.time));
     }
-  }).addTo(majorEarthquakes);
+  });
+  // .addTo(majorEarthquakes);
 
   // 8. Add the major earthquakes layer to the map
   // majorEarthquakes.addTo(map);
@@ -203,14 +213,13 @@ let legend = L.control({
 legend.onAdd = function() {
   let div = L.DomUtil.create("div", "info legend");
 
-  const magnitudes = [0, 1, 2, 4, 6.5, 8];
+  const magnitudes = [0, 2, 4, 6, 8];
   const colors = [
-    "#98ee00",
-    "#d4ee00",
-    "#eecc00",
-    "#ee9c00",
-    "#ea822c",
-    "#ea2c2c"
+    "#d9ff00",
+    "#a6ff00",
+    "#ffbf00",
+    "#ff8000",
+    "#ff4000"
   ];
 
 // Looping through our intervals to generate a label with a colored square for each interval.
